1afa74cd97c61d78cd76db829af13a54fe4f34b3
Kauã <kau.lira@yahoo.com>
2022-05-20 20:04:43 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿double contador, numero, produto;
+int multiplicador;
+
+Console.Write("Tabuada do número:");
+numero = Convert.ToDouble(Console.ReadLine()); 
+
+Console.Write("Digite até a multiplicação desejada:");
+multiplicador = Convert.ToInt32(Console.ReadLine());
+
+if( numero <0 || numero >10)
+{   
+    Console.WriteLine("Digite Numero de 0 a 10");
+}
+
+else
+{   
+    contador=0;
+
+while(contador <= multiplicador)
+{
+    produto = contador * numero;
+    Console.WriteLine($"{numero} x {contador} = {produto}");
+    contador +=1;
+}    
+
+}
